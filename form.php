5f08fe3235da6e7b7514bc960dcff782cd91edd3
SriAbhay <45917849+SriAbhay@users.noreply.github.com>
2022-04-05 00:05:07 +0530

    update php

--- form.php
+++ form.php
@@ -11,7 +11,7 @@ if (empty($name) || empty($Email) || empty(Address))
 }
 else
 {
-    mail("abhaybbk@gmail.com", "brewbox", $Address, "form: $name <$Email>");
+    mail("abhaybbk@gmail.com", "brewbox message", $Address, "form: $name <$Email>");
     echo "<script type='text/javascript'>alert('your order book successfully')
     windows.history.log(-1);
     </script>";
