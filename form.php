7381b5f73dbadbaaa9094992ac0b75da7775de23
SriAbhay <45917849+SriAbhay@users.noreply.github.com>
2022-04-04 23:36:54 +0530

    update form.php

--- form.php
+++ form.php
@@ -1,3 +1,19 @@
 <?php
-
+// variable setting
+$name = $_REQUEST['name'];
+$email = $_REQUEST['Email'];
+$name = $_REQUEST['Address'];
+ 
+//check feilds
+if (empty($name) || empty($Email) || empty(Address))
+{
+    echo "please fill all the feilds";
+}
+else
+{
+    mail("abhaybbk@gmail.com", "brewbox", $Address, "Form: $name <$email>");
+    echo "<script type='text/javascript'>alert('your order book successfully')
+    windows.history.log(-1);
+    </script>";
+}
 ?>
